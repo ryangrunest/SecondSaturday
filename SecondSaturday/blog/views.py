6244ac845586d330f6fd7040850ac4113a1677f9
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-22 10:52:53 -0700

    frontend folder structure

--- SecondSaturday/blog/views.py
+++ SecondSaturday/blog/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-  return HttpResponse("Hello, world. You're at the blog index.")
+# # Create your views here.
+# def index(request):
+#   return HttpResponse("Hello, world. You're at the blog index.")
+
+
+from blog.models import Blog
+from blog.serializers import BlogSerializer
+from rest_framework import generics
+
+class BlogListCreate(generics.ListCreateAPIView):
+  queryset = Blog.objects.all()
+  serializer_class = BlogSerializer
