e9d0f46c1eb2f937caf6acd352eb335c01620038
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-22 16:56:52 -0700

    more setup

--- SecondSaturday/SecondSaturday/urls.py
+++ SecondSaturday/SecondSaturday/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
+    # path('admin/', admin.site.urls),
 ]
