1dd7df3788d6884d7c14573b8cfaef08695fe57f
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-22 17:53:50 -0700

    react router init

--- SecondSaturday/SecondSaturday/urls.py
+++ SecondSaturday/SecondSaturday/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('blog.urls')),
     path('', include('frontend.urls')),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
